9555563576d8abab606dc3bf107b7fd38f8fc188
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2024-04-18 20:41:07 -0300

    configuracion de tailwindCss

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+export default {
+    plugins: {
+     // tailwindcss: {},
+      tailwindcss: {config: './tailwind.config.js' },
+      autoprefixer: {},
+    },
+  }
+  
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      colors: {
+        'blue': '#082F49"',
+        'green': '#4ADE80',
+        'esmerald': '#34D399',
+        'orange': '#F59E0B',
+        'gray': '#D6D3D1',
+        'light-gray': '#E5E5E5',
+        'black': '#000000',
+        'withe': '#FFFFFF'
+      },
+      fontFamily: {
+        'font-titles': ['Oswald', 'sans-serif'],
+        'font-text': ['Inter', 'sans-serif']
+      },
+      fontSize: {
+        'size-name': '32px',
+        'size-titles': '20px',
+        'size-subtitles': '15px',
+        'size-text': '16px'
+
+      },
+      extend: {},
+    },
+    plugins: [],
+  }
