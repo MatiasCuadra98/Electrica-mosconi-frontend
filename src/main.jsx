a6c348e0a1de21f4c7ff478fbf898bd60c112714
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2024-04-20 02:59:20 -0300

    tailwind sincronizado

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,14 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+
 // ReactDOM.render(
 //   <Provider store={store}>
 //     <BrowserRouter>
@@ -14,10 +22,3 @@ import store from "./redux/store.js";
 //   </Provider>,
 //   document.getElementById("root")
 // );
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-);
--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LandingPage = () => {
   return (
-    <div>
+    <div className="w-screen bg-slate-600">
       <h1>LANDING PAGE</h1>
       <h2>esta es una prueba</h2>
       <h3>para probar que tailwind esta ok</h3>
