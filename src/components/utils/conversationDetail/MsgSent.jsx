557efb6a8e9cc854ff892718467f1d827d331e36
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2024-07-04 11:50:20 -0300

    mensajes enviados desde nueva branch

--- src/components/utils/conversationDetail/MsgSent.jsx
+++ src/components/utils/conversationDetail/MsgSent.jsx
@@ -1,35 +1,11 @@
-// import React from "react";
-// import { useSelector } from "react-redux";
-// import IconUser from "../selectUser/IconUser";
-
-// const MsgSent = (props) => {
-//   //IMPORTANTE: se trae el dato de user, pero en realidad deberia traer el dato del usuario que contesto el mensaje, y no el activo en el momento!!!
-//   const user = useSelector((state) => state.user);
-//   return (
-//     <div className=" w-[473px] h-auto bg-stone-300 rounded-tr-[40px] rounded-bl-[40px] rounded-br-[40px] p-4 flex flex-col justify-between m-4 self-end mr-10">
-//       <div className="text-center mb-4">
-//         <h2 className="w-[432px] h-auto text-black text-base font-normal font-['Inter']">
-//           {props.props.message}
-//         </h2>
-//       </div>
-//       <div className="flex items-center justify-end gap-4">
-//         <p className="w-[74px] h-[18px] text-black text-[13px] font-normal font-['Oswald'] capitalize">
-//           {props.props.timestamp}
-//         </p>
-//         <IconUser name={user.name} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MsgSent;
-
 import React from "react";
 import { useSelector } from "react-redux";
 import IconUser from "../selectUser/IconUser";
 
 const MsgSent = (props) => {
-  const user = useSelector((state) => state.user);
+  const users = useSelector((state) => state.users);
+  const user = users.filter((user) => user.id === props.props.UserId);
+  const userName = user ? user.name : null;
   return (
     <div className="w-full flex justify-end">
       <div className="w-[473px] h-auto bg-stone-300 rounded-tr-[40px] rounded-bl-[40px] rounded-br-[40px] p-4 flex flex-col justify-between m-4 self-end mr-10">
@@ -42,7 +18,7 @@ const MsgSent = (props) => {
           <p className="w-[74px] h-[18px] text-black text-[13px] font-normal font-['Oswald'] capitalize">
             {props.props.timestamp}
           </p>
-          <IconUser name={user.name} />
+          <IconUser name={userName} />
         </div>
       </div>
     </div>
