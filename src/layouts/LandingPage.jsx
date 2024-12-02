196ce7cd6301e437806d1afb2f24db6d271b6443
Matías Cuadra <MatiasCuadra98>
2024-12-02 15:47:43 -0300

    log

--- src/layouts/LandingPage.jsx
+++ src/layouts/LandingPage.jsx
@@ -112,7 +112,7 @@ const LandingPage = () => {
             >
               INICIAR SESIÓN CON MERCADO LIBRE
             </a>
-             {/* este es el boton de fb, hay que meter la url en el .env y unir al boton de comencemos */}
+             {/* este es el boton de fb */}
             <a
               href={`https://electrica-mosconi-backend.onrender.com/auth/facebook`}
               className="w-fit h-fit  relative mx-auto px-8 py-1 bg-blue-600 hover:bg-blue-700 border-gray-700 rounded-[30px] shadow-inner text-white text-base font-normal font-['Oswald']"
