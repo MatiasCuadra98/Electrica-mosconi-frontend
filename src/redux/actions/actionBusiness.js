e95ebe1833bdf6eaabe5c026d84dd2779e5c0f23
Virginia <mvirginiamontoya@yahoo.com.ar>
2025-03-27 11:41:52 -0300

    cambio url del back

--- src/redux/actions/actionBusiness.js
+++ src/redux/actions/actionBusiness.js
@@ -14,9 +14,9 @@ import {
 //LOCALHOST
 //const URL = 'http://localhost:3000';
 //SERVER DESARROLLO
-//const URL = 'https://electrica-mosconi-backend.onrender.com';
+const URL = 'https://electrica-mosconi-backend.onrender.com';
 //SERVER PRODUCCION
-const URL ='https://electrica-mosconi-backend-main.onrender.com'
+//const URL ='https://electrica-mosconi-backend-main.onrender.com'
 
 export const getBusinessByIdAction = (businessId, businessName) => {
     const idBusiness = businessId || sessionStorage.getItem('businessId')
